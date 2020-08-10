beed4dbbe93767b21c2eb66fd364cf785b9c758a
Shane <sdoring1987@gmail.com>
2020-08-10 14:54:49 -0700

    implemented grounded and jump functions

--- Project4GPE/Assets/Scripts/Player.cs
+++ Project4GPE/Assets/Scripts/Player.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
+    public float speed=3;
+    public float jumpForce=5;
+    public int maxJumps = 1;
+    public int currentJumps;
+    public float height = 1.1f;
 
     private Rigidbody2D rigidBody;
     private SpriteRenderer sprite;
@@ -13,6 +17,7 @@ public class Player : MonoBehaviour
     {
         rigidBody = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        currentJumps = maxJumps;
     }
 
     // Update is called once per frame
@@ -31,5 +36,32 @@ public class Player : MonoBehaviour
             sprite.flipX = true;
         }
 
+
+        //jump 
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (IsGrounded())
+            {
+                currentJumps = maxJumps;
+            }
+            if (currentJumps > 0)
+            {
+                Jump();
+            }
+            
+        }
+
+        
+    }
+    void Jump()
+    {
+        currentJumps--;
+        rigidBody.AddForce(Vector2.up * jumpForce);
+    }
+    bool IsGrounded()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, (height / 2f) * 0.1f);
+
+        return (hitInfo.collider!=null);
     }
 }
